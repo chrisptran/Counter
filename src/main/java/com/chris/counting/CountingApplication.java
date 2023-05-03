e951459fd770a84d3ca6f9921033bdca81f5f346
chrisptran <christopher.p.tran@gmail.com>
2023-05-02 23:20:10 -0700

    first commit

---
+++ src/main/java/com/chris/counting/CountingApplication.java
@@ -0,0 +1,13 @@
+package com.chris.counting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountingApplication.class, args);
+	}
+
+}
